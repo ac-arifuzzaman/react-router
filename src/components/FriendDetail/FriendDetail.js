7b3079e3735efe4e36bf529095167519c0e0562a
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2022-12-30 00:27:12 +0600

    add some state

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
     const { friendId } = useParams()
+    const [friend, setFriend] = useState({});
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
         <div>
             <h1>hello form dosto er bari: {friendId}</h1>
