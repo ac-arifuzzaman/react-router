2b6286119c2d61b127c2fb88284eda066488ef03
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2022-12-30 11:25:54 +0600

    add and set dynamic data

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,20 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const FriendDetail = () => {
-    const { friendId } = useParams()
-    const [friend, setFriend] = useState({});
-    useEffect(() => {
-        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setFriend(data))
-    }, [])
-    return (
-        <div>
-            <h1>hello form dosto er bari: {friendId}</h1>
-        </div>
-    );
+  // const params = useParams()
+  const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
+  return (
+    <div>
+      <h1>hello form dosto er bari: {friendId}</h1>
+      <h2>Name: {friend.name}</h2>
+      <p>
+        <small>Email: {friend.email}</small>
+      </p>
+      <p>Address: {friend.address?.city}</p>
+      <p>
+        <small>Geo location: {friend.address?.geo.lat}</small>
+      </p>
+    </div>
+  );
 };
 
 export default FriendDetail;
